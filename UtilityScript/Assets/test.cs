3aa083911c76e9b0c100764f28af8eced170313d
keito.N <mgzrx39@gmail.com>
2019-05-27 01:04:56 +0900

    no message

---
+++ UtilityScript/Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField, SelectBGM] string a;
+    [SerializeField, SelectBGM] string b;
+    void Start()
+    {
+        
+    }
+    public void ButaanA()
+    {
+        AudioManeger.Instance.PlayBgm(a);
+    }
+    public void ButaanB()
+    {
+        AudioManeger.Instance.PlayBgm(b);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
